4564e258719942bcb0d1b594418508d34f6e8274
Vesert <vesert1@gmail.com>
2018-10-03 00:12:10 +0400

    Update present_type.py

--- types/present_type.py
+++ types/present_type.py
@@ -6,12 +6,12 @@ my_tuple = (2,3)
 my_dict = {'name' : 'Paul', 'What can i do' : 'I can wish you a happy birthday'}
 my_set = set([2,3,2,5])
 
-print('Тип числа ', my_num , type(my_num))
-print('Тип строки \'' + str + '\' ', type(str))
-print('Тип списка ', my_list, ' ',type(my_list))
-print('Тип кортежа ', my_tuple, ' ',type(my_tuple))
-print('Тип словаря ', my_dict, ' ',type(my_dict))
-print('Тип множества ', my_set, ' ',type(my_set))
+print('Тип числа ', my_num , str(type(my_num)))
+print('Тип строки \'' + str + '\' ', str(type(str)))
+print('Тип списка ', my_list, ' ',str(type(my_list)))
+print('Тип кортежа ', my_tuple, ' ',str(type(my_tuple)))
+print('Тип словаря ', my_dict, ' ',str(type(my_dict)))
+print('Тип множества ', my_set, ' ',str(type(my_set)))
 
 # Также есть объекты, но их рассмотрим отдельно
 
