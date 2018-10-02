66b47e8f92ccb5202bd0b8d0489abfd887949935
Vesert <vesert1@gmail.com>
2018-10-03 00:07:03 +0400

    Create present_type.py

---
+++ types/present_type.py
@@ -0,0 +1,17 @@
+"""typeps of data"""
+my_str = 'str'
+my_num = 1
+my_list = [2,3,4]
+my_tuple = (2,3)
+my_dict = {'name' 'Paul', 'What can i do' : 'I can wish you a happy birthday'}
+my_set = set(2,3,2,5)
+
+print('Тип числа ' , type(my_num))
+print('Тип строки \'' + str + '\' ', type(str))
+print('Тип списка ', my_list, ' ',type(my_list))
+print('Тип кортежа ', my_tuple, ' ',type(my_tuple))
+print('Тип словаря ', my_dict, ' ',type(my_dict))
+print('Тип множества ', my_set, ' ',type(my_set))
+
+# Также есть объекты, но их рассмотрим отдельно
+
