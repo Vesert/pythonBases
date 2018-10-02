4f1e5eb16c8a86d1d8f44d64ed22a6d5ecae1bc3
Vesert <vesert1@gmail.com>
2018-10-03 00:13:06 +0400

    Update present_type.py

--- types/present_type.py
+++ types/present_type.py
@@ -7,7 +7,7 @@ my_dict = {'name' : 'Paul', 'What can i do' : 'I can wish you a happy birthday'}
 my_set = set([2,3,2,5])
 
 print('Тип числа ', my_num , str(type(my_num)))
-print('Тип строки \'' + str + '\' ', str(type(str)))
+print('Тип строки \'' + str + '\' ', str(type(my_str)))
 print('Тип списка ', my_list, ' ',str(type(my_list)))
 print('Тип кортежа ', my_tuple, ' ',str(type(my_tuple)))
 print('Тип словаря ', my_dict, ' ',str(type(my_dict)))
